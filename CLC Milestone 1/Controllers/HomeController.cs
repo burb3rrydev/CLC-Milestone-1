e659ca51ab4e3362a0b37277d30845385a756ede
burb3rrydev <112503617+burb3rrydev@users.noreply.github.com>
2024-05-04 14:52:33 -0600

    Add project files.

---
+++ CLC Milestone 1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CLC_Milestone_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubmitForm(string FirstName, string LastName, string Sex, int Age, string State, string Email, string Username, string Password)
+        {
+            // Process the form data here
+
+            // just return a success message
+            return Content("Form submitted successfully!");
+        }
+
+    }
+}
